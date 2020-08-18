b16a4ca5f0edf9452fcdd74296b28395f8355636
kwk9892 <kwk9892@gmail.com>
2020-08-18 15:27:21 +0800

    added dev

---
+++ dev/home/templatetags/custom_tags.py
@@ -0,0 +1,13 @@
+from django import template
+register = template.Library()
+
+@register.filter
+def index(indexable, i):
+    return indexable[i]
+
+@register.simple_tag(takes_context=True)
+def url_replace(context, **kwargs):
+    query = context['request'].GET.copy()
+    for k, v in kwargs.items():
+        query[k] = v
+    return query.urlencode()
